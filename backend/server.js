01bd165eb938e45633f9057220d2d7cd5ba69cff
shaileshj121 <shaileshbisht9876@gmail.com>
2023-12-27 20:04:48 +0530

    import error fix

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,11 @@
-import 'dotenv/config'
+import 'dotenv/config';
 import express from 'express';
 
 const app = express();
 
 app.get('/', (req, res) => {
     res.send('server is ready')
-})
+});
  
 const Port = process.env.PORT || 3000;
 
